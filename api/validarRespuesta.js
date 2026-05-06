db6c1db5908c83850388581aba89cf92dd8548e9
tierrasagrada <122334725+tierrasagrada@users.noreply.github.com>
2026-05-06 18:10:47 -0400

    Update validarRespuesta.js

--- api/validarRespuesta.js
+++ api/validarRespuesta.js
@@ -161,7 +161,7 @@ const limiter = rateLimit({
                           <i class="fa fa-facebook"></i>
                         </a>
                         <a class="fa-icon fa-icon-2x" href="https://x.com/" title="">                                                      
-                          <i class="fa-brands fa-x-twitter"></i>
+                          <i class="fa fa-twitter"></i>
                         </a>
                         <a class="fa-icon fa-icon-2x" href="https://dribbble.com/" title="">
                           <i class="fa fa-dribbble"></i>
